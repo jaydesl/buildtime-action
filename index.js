970b323a79cbd40f881530faf1b588da7e23e799
jaydesl <jay.deslauriers@gmail.com>
2020-01-13 15:32:43 +0000

    f

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function run() {
       owner,
       repo
     });
-    console.log(commitList.data.sort(function(a,b){return a.created_at - b.created_at;}));
+    console.log(commitList.data.sort(function(a,b){return b.created_at - a.created_at;}));
 
     const run_list = mylist.data.check_runs
     var workflow;
